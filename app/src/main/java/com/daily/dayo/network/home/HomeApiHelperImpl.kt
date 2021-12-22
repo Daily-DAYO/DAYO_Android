fa1cec5f73df5cc0c9b8af60a008dd3eb4ce8085
DongJun-H <dongjun826@seoultech.ac.kr>
2021-12-23 00:04:09 +0900

    #66 [feature] Apply Hilt, Retrofit2 base
    resolved: #66

--- app/src/main/java/com/daily/dayo/network/home/HomeApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/home/HomeApiHelperImpl.kt
@@ -1,8 +1,16 @@
 package com.daily.dayo.network.home
 
 import com.daily.dayo.home.model.ResponseHomePost
+import retrofit2.Response
 import javax.inject.Inject
 
-class HomeApiHelperImpl @Inject constructor(private val homeApiService: HomeApiService) : HomeApiService {
-    override suspend fun requestPostList(): ResponseHomePost = homeApiService.requestPostList()
+class HomeApiHelperImpl @Inject constructor(
+    private val homeApiService: HomeApiService) : HomeApiHelper {
+    // @Inject constructor을 통해 생성자에 Retrofit2 Interface 주입
+    // ApiHelperImpl will be the class which will implement ApiHelper to provide functionality to ApiHelper functions.
+
+    // In ApiHelperImpl we have injected ApiService in the constructor itself
+    // So we do not have to create new instance of ApiService instead it is passed as a dependency to the ApiHelperImpl.
+    // In this way we can have only one instance of ApiService throughout the application lifecycle for any number of network calls.
+    override suspend fun requestPostList(): Response<ResponseHomePost> = homeApiService.requestPostList()
 }
--- app/src/main/java/com/daily/dayo/repository/HomeRepository.kt
+++ app/src/main/java/com/daily/dayo/repository/HomeRepository.kt
@@ -1,17 +1,22 @@
 package com.daily.dayo.repository
 
+import com.daily.dayo.home.model.ResponseHomePost
+import com.daily.dayo.network.home.HomeApiHelper
 import com.daily.dayo.network.home.HomeApiHelperImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
 import javax.inject.Inject
 
-class HomeRepository @Inject constructor(private val homeApiHelperImpl: HomeApiHelperImpl) {
-    suspend fun requestPostList() = flow {
-        runCatching {
-            homeApiHelperImpl.requestPostList()
-        }.getOrNull()?.let { postList ->
-            emit(postList.posts)
-        } ?: emit(null)
-    }.flowOn(Dispatchers.IO)
+class HomeRepository @Inject constructor(private val homeApiHelper: HomeApiHelper) {
+    suspend fun requestPostList() = homeApiHelper.requestPostList().verify()
+}
+
+fun Response<ResponseHomePost>.verify() : Response<ResponseHomePost> {
+    if (this.isSuccessful && this.code() in 200..299) {
+        return this
+    } else {
+        throw Exception("${this.code()}")
+    }
 }
