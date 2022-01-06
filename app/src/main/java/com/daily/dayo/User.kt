0caf98fbc279a2c5b1ed9abb65f98fa9f0534312
DongJun-H <dongjun826@seoultech.ac.kr>
2022-01-06 23:28:00 +0900

    #92 [bug] LoginActivity, KAKAO Login Fail
    resolved: #92

--- app/src/main/java/com/daily/dayo/User.kt
+++ app/src/main/java/com/daily/dayo/User.kt
@@ -1,5 +1,7 @@
 package com.daily.dayo
 
 data class User(
-    var id:String?=null
+    var id:String?=null,
+    var accessToken:String?=null,
+    var refreshToken:String?=null
 )
--- app/src/main/java/com/daily/dayo/login/LoginViewModel.kt
+++ app/src/main/java/com/daily/dayo/login/LoginViewModel.kt
@@ -1,28 +1,17 @@
 package com.daily.dayo.login
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel(
-    private val loginRepository: LoginRepository
-) : ViewModel(){
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository) : ViewModel(){
 
-    fun loginRequest(request: LoginRequest) {
+    fun loginRequest(request: LoginRequest) =
         viewModelScope.launch (Dispatchers.IO){
             loginRepository.requestLogin(request)
         }
-    }
-}
-
-class LoginViewModelFactory(private val loginRepository: LoginRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return LoginViewModel(loginRepository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
 }
