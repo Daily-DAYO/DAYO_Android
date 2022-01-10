42b0bf37c828ff60f23e91b3e53a137cd3f02733
DongJun-H <dongjun826@seoultech.ac.kr>
2022-01-10 14:55:09 +0900

    #98 [feature] Write Comment, Read Comment List
    resolved: #98

--- app/src/main/java/com/daily/dayo/network/Post/PostApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/Post/PostApiHelperImpl.kt
@@ -1,9 +1,14 @@
 package com.daily.dayo.network.Post
 
+import com.daily.dayo.post.model.RequestCreatePostComment
+import com.daily.dayo.post.model.ResponseCreatePostComment
 import com.daily.dayo.post.model.ResponsePost
+import com.daily.dayo.post.model.ResponsePostComment
 import retrofit2.Response
 import javax.inject.Inject
 
 class PostApiHelperImpl @Inject constructor(private val postApiService: PostApiService) : PostApiHelper {
     override suspend fun requestPostDetail(postId: Int): Response<ResponsePost> = postApiService.requestPostDetail(postId)
+    override suspend fun requestPostComment(postId: Int): Response<ResponsePostComment> = postApiService.requestPostComment(postId)
+    override suspend fun requestCreatePostComment(request: RequestCreatePostComment): Response<ResponseCreatePostComment> = postApiService.requestCreatePostComment(request)
 }
--- app/src/main/java/com/daily/dayo/repository/PostRepository.kt
+++ app/src/main/java/com/daily/dayo/repository/PostRepository.kt
@@ -1,14 +1,37 @@
 package com.daily.dayo.repository
 
 import com.daily.dayo.network.Post.PostApiHelper
+import com.daily.dayo.post.model.RequestCreatePostComment
+import com.daily.dayo.post.model.ResponseCreatePostComment
 import com.daily.dayo.post.model.ResponsePost
+import com.daily.dayo.post.model.ResponsePostComment
 import retrofit2.Response
 import javax.inject.Inject
 
 class PostRepository @Inject constructor(private val postApiHelper: PostApiHelper) {
     suspend fun requestPostDetail(postId: Int) = postApiHelper.requestPostDetail(postId).verify()
+    suspend fun requestPostComment(postId: Int) = postApiHelper.requestPostComment(postId).verify()
+    suspend fun requestCreatePostComment(request: RequestCreatePostComment) = postApiHelper.requestCreatePostComment(request).verify()
 
     fun Response<ResponsePost>.verify() : Response<ResponsePost> {
+        if (this.isSuccessful && this.code() in 200..299) {
+            return this
+        } else {
+            throw Exception("${this.code()}")
+        }
+    }
+
+    @JvmName("verifyResponsePostComment")
+    fun Response<ResponsePostComment>.verify() : Response<ResponsePostComment> {
+        if (this.isSuccessful && this.code() in 200..299) {
+            return this
+        } else {
+            throw Exception("${this.code()}")
+        }
+    }
+
+    @JvmName("verifyResponseCreatePostComment")
+    fun Response<ResponseCreatePostComment>.verify() : Response<ResponseCreatePostComment> {
         if (this.isSuccessful && this.code() in 200..299) {
             return this
         } else {
