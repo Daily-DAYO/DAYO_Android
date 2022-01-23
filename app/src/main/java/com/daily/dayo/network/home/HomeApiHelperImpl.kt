b10e3986d7d4233656647b48e5ab302a99ceb2d7
DongJun-H <dongjun826@seoultech.ac.kr>
2022-01-24 03:26:22 +0900

    #124 [feature] HomePostItem
    resolved: #124

--- app/src/main/java/com/daily/dayo/network/home/HomeApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/home/HomeApiHelperImpl.kt
@@ -1,11 +1,12 @@
 package com.daily.dayo.network.home
 
+import com.daily.dayo.home.model.RequestLikePost
 import com.daily.dayo.home.model.ResponseHomePost
+import com.daily.dayo.home.model.ResponseLikePost
 import retrofit2.Response
 import javax.inject.Inject
 
-class HomeApiHelperImpl @Inject constructor(
-    private val homeApiService: HomeApiService) : HomeApiHelper {
+class HomeApiHelperImpl @Inject constructor(private val homeApiService: HomeApiService) : HomeApiHelper {
     // @Inject constructor을 통해 생성자에 Retrofit2 Interface 주입
     // ApiHelperImpl will be the class which will implement ApiHelper to provide functionality to ApiHelper functions.
 
@@ -13,4 +14,6 @@ class HomeApiHelperImpl @Inject constructor(
     // So we do not have to create new instance of ApiService instead it is passed as a dependency to the ApiHelperImpl.
     // In this way we can have only one instance of ApiService throughout the application lifecycle for any number of network calls.
     override suspend fun requestPostList(): Response<ResponseHomePost> = homeApiService.requestPostList()
+    override suspend fun requestLikePost(request: RequestLikePost): Response<ResponseLikePost> = homeApiService.requestLikePost(request)
+    override suspend fun requestUnlikePost(postId: Int): Response<Void> = homeApiService.requestUnlikePost(postId)
 }
--- app/src/main/java/com/daily/dayo/network/home/HomeApiService.kt
+++ app/src/main/java/com/daily/dayo/network/home/HomeApiService.kt
@@ -1,12 +1,18 @@
 package com.daily.dayo.network.home
 
+import com.daily.dayo.home.model.RequestLikePost
 import com.daily.dayo.home.model.ResponseHomePost
+import com.daily.dayo.home.model.ResponseLikePost
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface HomeApiService {
     // (HomeApiService will service our network call)
     // 네트워크 작업 Call
     @GET("/api/v1/posts")
     suspend fun requestPostList(): Response<ResponseHomePost>
+    @POST("/api/v1/heart")
+    suspend fun requestLikePost(@Body body : RequestLikePost) : Response<ResponseLikePost>
+    @POST("/api/v1/heart/delete/{postId}")
+    suspend fun requestUnlikePost(@Path("postId") postId : Int) : Response<Void>
 }
