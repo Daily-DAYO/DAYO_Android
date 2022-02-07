e14986bfe6b9d0216971ae95cfb5fa666bdf54ae
DongJun-H <dongjun826@seoultech.ac.kr>
2022-02-07 21:33:39 +0900

    #151 [feature] LoginFragment with Email Signup
    resolved: #151

--- app/src/main/java/com/daily/dayo/login/LoginViewModel.kt
+++ app/src/main/java/com/daily/dayo/login/LoginViewModel.kt
@@ -1,17 +1,22 @@
 package com.daily.dayo.login
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.daily.dayo.util.Resource
+import com.daily.dayo.login.model.SignupEmailRequest
+import com.daily.dayo.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository) : ViewModel(){
+
+    private val _signupSuccess = MutableLiveData<Event<Boolean>>()
+    val signupSuccess : LiveData<Event<Boolean>> get() =_signupSuccess
 
     fun requestLogin(request: LoginRequest) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -23,4 +28,12 @@ class LoginViewModel @Inject constructor(private val loginRepository: LoginRepos
         loginRepository.requestMemberInfo()
     }
 
+    fun requestSignupEmail(email: String, nickname: String, password: String, profileImg: File?) = viewModelScope.launch(Dispatchers.IO) {
+        val response = loginRepository.requestSignupEmail(email, nickname, password, profileImg)
+        if (response.isSuccessful) {
+            _signupSuccess.postValue(Event(true))
+        } else {
+            _signupSuccess.postValue(Event(false))
+        }
+    }
 }
--- app/src/main/java/com/daily/dayo/network/login/LoginApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/login/LoginApiHelperImpl.kt
@@ -2,6 +2,9 @@ package com.daily.dayo.network.login
 import com.daily.dayo.login.LoginRequest
 import com.daily.dayo.login.LoginResponse
 import com.daily.dayo.login.MemberResponse
+import com.daily.dayo.login.model.SignupEmailRequest
+import com.daily.dayo.login.model.SignupEmailResponse
+import okhttp3.MultipartBody
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -12,4 +15,11 @@ class LoginApiHelperImpl@Inject constructor(private val loginApiService: LoginAp
 
     override suspend fun requestMemberInfo() : Response<MemberResponse> =
         loginApiService.requestMemberInfo()
+
+    override suspend fun requestSignupEmail(
+        email: String,
+        nickname: String,
+        password: String,
+        profileImg: MultipartBody.Part
+    ): Response<SignupEmailResponse> = loginApiService.requestSignupEmail(email, nickname, password, profileImg)
 }
