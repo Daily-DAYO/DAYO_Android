0ff25df55acd39ede8da1b79630219852db3411f
DongJun-H <dongjun826@seoultech.ac.kr>
2022-02-21 00:06:34 +0900

    #178 [feature] Post Bookmarking
    resolved: #178

--- app/src/main/java/com/daily/dayo/network/post/PostApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/post/PostApiHelperImpl.kt
@@ -9,6 +9,8 @@ class PostApiHelperImpl @Inject constructor(private val postApiService: PostApiS
     override suspend fun requestDeletePost(postId: Int): Response<Void> = postApiService.requestDeletePost(postId)
     override suspend fun requestLikePost(request: RequestLikePost): Response<ResponseLikePost> = postApiService.requestLikePost(request)
     override suspend fun requestUnlikePost(postId: Int): Response<Void> = postApiService.requestUnlikePost(postId)
+    override suspend fun requestBookmarkPost(request: RequestBookmarkPost): Response<ResponseBookmarkPost> = postApiService.requestBookmarkPost(request)
+    override suspend fun requestDeleteBookmarkPost(postId: Int): Response<Void> = postApiService.requestDeleteBookmarkPost(postId)
     override suspend fun requestPostComment(postId: Int): Response<ResponsePostComment> = postApiService.requestPostComment(postId)
     override suspend fun requestCreatePostComment(request: RequestCreatePostComment): Response<ResponseCreatePostComment> = postApiService.requestCreatePostComment(request)
     override suspend fun requestDeletePostComment(commentId: Int): Response<Void> = postApiService.requestDeletePostComment(commentId)
