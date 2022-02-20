d5dbae4c15fa16f1934198b7af3201d2436a996d
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-02-21 01:56:06 +0900

    #171 [feature] ProfileBookmarkListFragment resolved: #171

--- app/src/main/java/com/daily/dayo/network/profile/ProfileApiHelper.kt
+++ app/src/main/java/com/daily/dayo/network/profile/ProfileApiHelper.kt
@@ -1,5 +1,6 @@
 package com.daily.dayo.network.profile
 
+import com.daily.dayo.profile.model.ResponseBookmarkPostList
 import com.daily.dayo.profile.model.ResponseLikePostList
 import com.daily.dayo.profile.model.ResponseMyProfile
 import com.daily.dayo.profile.model.ResponseOtherProfile
@@ -11,4 +12,5 @@ interface ProfileApiHelper {
     suspend fun requestUpdateMyProfile(nickname: String?, profileImg: MultipartBody.Part?) : Response<Void>
     suspend fun requestOtherProfile(memberId:String):Response<ResponseOtherProfile>
     suspend fun requestAllMyLikePostList() : Response<ResponseLikePostList>
+    suspend fun requestAllMyBookmarkPostList() : Response<ResponseBookmarkPostList>
 }
--- app/src/main/java/com/daily/dayo/network/profile/ProfileApiHelperImpl.kt
+++ app/src/main/java/com/daily/dayo/network/profile/ProfileApiHelperImpl.kt
@@ -1,5 +1,6 @@
 package com.daily.dayo.network.profile
 
+import com.daily.dayo.profile.model.ResponseBookmarkPostList
 import com.daily.dayo.profile.model.ResponseLikePostList
 import com.daily.dayo.profile.model.ResponseMyProfile
 import com.daily.dayo.profile.model.ResponseOtherProfile
@@ -12,4 +13,5 @@ class ProfileApiHelperImpl @Inject constructor(private val profileApiService: Pr
    override suspend fun requestUpdateMyProfile(nickname: String?, profileImg: MultipartBody.Part?): Response<Void> = profileApiService.requestUpdateMyProfile(nickname, profileImg)
    override suspend fun requestOtherProfile(memberId:String):Response<ResponseOtherProfile> = profileApiService.requestOtherProfile(memberId)
    override suspend fun requestAllMyLikePostList(): Response<ResponseLikePostList> = profileApiService.requestAllMyLikePostList()
+   override suspend fun requestAllMyBookmarkPostList(): Response<ResponseBookmarkPostList> = profileApiService.requestAllMyBookmarkPostList()
 }
