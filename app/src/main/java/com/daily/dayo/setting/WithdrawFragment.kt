8974dd1c7c690ff3f2928a36483c3fefe9a41721
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-05-24 18:44:27 +0900

    #242 [feature] Rebasing Refactoring Code

--- app/src/main/java/com/daily/dayo/setting/WithdrawFragment.kt
+++ app/src/main/java/com/daily/dayo/setting/WithdrawFragment.kt
@@ -1,5 +1,6 @@
 package com.daily.dayo.setting
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Spannable
 import android.text.style.ForegroundColorSpan
@@ -8,13 +9,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.daily.dayo.DayoApplication
 import com.daily.dayo.R
+import com.daily.dayo.SharedManager
 import com.daily.dayo.databinding.FragmentWithdrawBinding
+import com.daily.dayo.login.LoginActivity
+import com.daily.dayo.login.LoginViewModel
 import com.daily.dayo.util.autoCleared
 
 class WithdrawFragment : Fragment() {
     private var binding by autoCleared<FragmentWithdrawBinding>()
+    private val loginViewModel by activityViewModels<LoginViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,22 +44,39 @@ class WithdrawFragment : Fragment() {
 
     private fun setOnClickWithdrawOtherReason(){
         binding.radiogroupWithdrawReason.setOnCheckedChangeListener { _, id ->
-            if(binding.radiogroupWithdrawReason.checkedRadioButtonId!=-1) binding.btnWithdraw.isSelected = true
+            if(binding.radiogroupWithdrawReason.checkedRadioButtonId!=-1) binding.btnWithdraw.isEnabled = true
             when(id) {
                 binding.radiobuttonWithdrawReasonOther.id -> binding.etWithdrawReasonOther.visibility = View.VISIBLE
-                else -> binding.etWithdrawReasonOther.visibility = View.INVISIBLE
+                else -> binding.etWithdrawReasonOther.visibility = View.GONE
             }
         }
     }
 
     private fun setWithdrawButtonActivation(){
-        if(binding.radiogroupWithdrawReason.checkedRadioButtonId==-1) binding.btnWithdraw.isSelected = false
+        if(binding.radiogroupWithdrawReason.checkedRadioButtonId==-1) {
+            binding.btnWithdraw.isEnabled = false
+        }
     }
 
     private fun setWithdrawButtonClickListener(){
         binding.btnWithdraw.setOnClickListener {
-            // TODO : 회원 탈퇴 관련 API 호출 (탈퇴 및 사유 저장)
-            //  로그인 정보 삭제 후 로그인 화면으로 이동
+            val reason = when(binding.radiogroupWithdrawReason.checkedRadioButtonId){
+                R.id.radiobutton_withdraw_reason_1 -> binding.radiobuttonWithdrawReason1.text.toString()
+                R.id.radiobutton_withdraw_reason_2 -> binding.radiobuttonWithdrawReason2.text.toString()
+                R.id.radiobutton_withdraw_reason_3 -> binding.radiobuttonWithdrawReason3.text.toString()
+                R.id.radiobutton_withdraw_reason_4 -> binding.radiobuttonWithdrawReason4.text.toString()
+                R.id.radiobutton_withdraw_reason_5 -> binding.radiobuttonWithdrawReason5.text.toString()
+                else -> binding.radiobuttonWithdrawReasonOther.text.toString()
+            }
+            loginViewModel.requestWithdraw(reason)
+            loginViewModel.withdrawSuccess.observe(viewLifecycleOwner){
+                if(it) {
+                    SharedManager(DayoApplication.applicationContext()).clearPreferences()
+                    val intent = Intent(this.activity, LoginActivity::class.java)
+                    startActivity(intent)
+                    this.requireActivity().finish()
+                }
+            }
         }
     }
 
