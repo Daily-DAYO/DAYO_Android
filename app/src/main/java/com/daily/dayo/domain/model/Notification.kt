f2c78a61faa462ff2cf7bf12b7fc43d05771017c
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-06-22 04:03:19 +0900

    #255 [feature] Notification Fragment
    resolved: #255

--- app/src/main/java/com/daily/dayo/domain/model/Notification.kt
+++ app/src/main/java/com/daily/dayo/domain/model/Notification.kt
@@ -1,5 +1,12 @@
 package com.daily.dayo.domain.model
 
 data class Notification(
-    val id: Int
+    val alarmId:Int?,
+    val topic: Topic?,
+    val check: Boolean?,
+    val content: String?,
+    val createdTime: String?,
+    val nickname: String?,
+    val memberId: String?,
+    val postId: Int?
 )
---
+++ app/src/main/java/com/daily/dayo/domain/model/Topic.kt
@@ -0,0 +1,5 @@
+package com.daily.dayo.domain.model
+
+enum class Topic {
+    HEART, COMMENT, NOTICE, FOLLOW
+}
