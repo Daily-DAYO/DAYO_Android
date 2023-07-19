037efe1a73adbd9925e4075ca3964f372043016e
DongJun-H <dongjun826@seoultech.ac.kr>
2023-07-19 11:57:11 +0900

    [bug] Fix delayed reaction occurs when the reaction button is clicked

--- app/src/main/java/com/daily/dayo/presentation/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/daily/dayo/presentation/viewmodel/FeedViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.map
 import com.daily.dayo.common.Resource
 import com.daily.dayo.data.datasource.remote.bookmark.CreateBookmarkRequest
 import com.daily.dayo.data.datasource.remote.bookmark.CreateBookmarkResponse
@@ -19,6 +20,7 @@ import com.daily.dayo.domain.usecase.like.RequestLikePostUseCase
 import com.daily.dayo.domain.usecase.like.RequestUnlikePostUseCase
 import com.daily.dayo.domain.usecase.post.RequestFeedListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -41,16 +43,30 @@ class FeedViewModel @Inject constructor(
     private val _postBookmarked = MutableLiveData<Resource<CreateBookmarkResponse>>()
     val postBookmarked: LiveData<Resource<CreateBookmarkResponse>> get() = _postBookmarked
 
-    fun requestFeedList() = viewModelScope.launch {
+    fun requestFeedList() = viewModelScope.launch(Dispatchers.IO) {
         requestFeedListUseCase()
             .cachedIn(viewModelScope)
             .collectLatest { _feedList.postValue(it) }
     }
 
-    fun requestLikePost(postId: Int) = viewModelScope.launch {
+    fun requestLikePost(postId: Int) = viewModelScope.launch(Dispatchers.IO) {
         requestLikePostUseCase(CreateHeartRequest(postId = postId))?.let { ApiResponse ->
             when (ApiResponse) {
-                is NetworkResponse.Success -> { _postLiked.postValue(Resource.success(ApiResponse.body)) }
+                is NetworkResponse.Success -> {
+                    _feedList.postValue(
+                        _feedList.value?.map {
+                            if (it.postId == postId) {
+                                it.copy(
+                                    heart = true,
+                                    heartCount = ApiResponse.body?.allCount ?: 0
+                                )
+                            } else {
+                                it
+                            }
+                        }
+                    )
+                    _postLiked.postValue(Resource.success(ApiResponse.body))
+                }
                 is NetworkResponse.NetworkError -> { _postLiked.postValue(Resource.error(ApiResponse.exception.toString(), null)) }
                 is NetworkResponse.ApiError -> { _postLiked.postValue(Resource.error(ApiResponse.error.toString(), null)) }
                 is NetworkResponse.UnknownError -> { _postLiked.postValue(Resource.error(ApiResponse.throwable.toString(), null)) }
@@ -58,14 +74,45 @@ class FeedViewModel @Inject constructor(
         }
     }
 
-    fun requestUnlikePost(postId: Int) = viewModelScope.launch {
-        requestUnlikePostUseCase(postId = postId)
+    fun requestUnlikePost(postId: Int) = viewModelScope.launch(Dispatchers.IO) {
+        requestUnlikePostUseCase(postId = postId)?.let { ApiResponse ->
+            when (ApiResponse) {
+                is NetworkResponse.Success -> {
+                    _feedList.postValue(
+                        _feedList.value?.map {
+                            if (it.postId == postId) {
+                                it.copy(
+                                    heart = false,
+                                    heartCount = ApiResponse.body?.allCount ?: 0
+                                )
+                            } else {
+                                it
+                            }
+                        }
+                    )
+                }
+                else -> {}
+            }
+        }
     }
 
-    fun requestBookmarkPost(postId: Int) = viewModelScope.launch {
+    fun requestBookmarkPost(postId: Int) = viewModelScope.launch(Dispatchers.IO) {
         requestBookmarkPostUseCase(CreateBookmarkRequest(postId = postId)).let { ApiResponse ->
             when (ApiResponse) {
-                is NetworkResponse.Success -> { _postBookmarked.postValue(Resource.success(ApiResponse.body)) }
+                is NetworkResponse.Success -> {
+                    _feedList.postValue(
+                        _feedList.value?.map {
+                            if (it.postId == postId) {
+                                it.copy(
+                                    bookmark = true
+                                )
+                            } else {
+                                it
+                            }
+                        }
+                    )
+                    _postBookmarked.postValue(Resource.success(ApiResponse.body))
+                }
                 is NetworkResponse.NetworkError -> { _postBookmarked.postValue(Resource.error(ApiResponse.exception.toString(), null)) }
                 is NetworkResponse.ApiError -> { _postBookmarked.postValue(Resource.error(ApiResponse.error.toString(), null)) }
                 is NetworkResponse.UnknownError -> { _postBookmarked.postValue(Resource.error(ApiResponse.throwable.toString(), null)) }
@@ -73,7 +120,24 @@ class FeedViewModel @Inject constructor(
         }
     }
 
-    fun requestDeleteBookmarkPost(postId: Int) = viewModelScope.launch {
-        requestDeleteBookmarkPostUseCase(postId = postId)
+    fun requestDeleteBookmarkPost(postId: Int) = viewModelScope.launch(Dispatchers.IO) {
+        requestDeleteBookmarkPostUseCase(postId = postId)?.let { ApiResponse ->
+            when(ApiResponse) {
+                is NetworkResponse.Success -> {
+                    _feedList.postValue(
+                        _feedList.value?.map {
+                            if (it.postId == postId) {
+                                it.copy(
+                                    bookmark = false
+                                )
+                            } else {
+                                it
+                            }
+                        }
+                    )
+                }
+                else -> {}
+            }
+        }
     }
 }
