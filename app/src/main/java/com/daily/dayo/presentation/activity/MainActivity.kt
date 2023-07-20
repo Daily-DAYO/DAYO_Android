5e72187fcbc28a392b6f5046a825533cea2820d2
DongJun-H <dongjun826@seoultech.ac.kr>
2023-07-20 12:41:16 +0900

    [feature] Apply changes as update to Android SDK 3

--- app/src/main/java/com/daily/dayo/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/daily/dayo/presentation/activity/MainActivity.kt
@@ -1,41 +1,149 @@
 package com.daily.dayo.presentation.activity
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.forEach
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.daily.dayo.DayoApplication
 import com.daily.dayo.R
 import com.daily.dayo.databinding.ActivityMainBinding
 import com.daily.dayo.presentation.fragment.home.HomeFragmentDirections
+import com.daily.dayo.presentation.viewmodel.SettingNotificationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val settingNotificationViewModel by viewModels<SettingNotificationViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkCurrentNotification()
         initBottomNavigation()
         setBottomNaviVisibility()
         disableBottomNaviTooltip()
         getNotificationData()
+        askNotificationPermission()
     }
 
-    private fun getNotificationData(){
+    private fun checkCurrentNotification() {
+        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+            Toast.makeText(
+                this,
+                getString(R.string.permission_fail_message_notification),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun getNotificationData() {
         val extraFragment = intent.getStringExtra("ExtraFragment")
-        if(extraFragment!=null && extraFragment == "Notification"){
+        if (extraFragment != null && extraFragment == "Notification") {
             val postId = intent.getStringExtra("PostId")?.toInt()
             val memberId = intent.getStringExtra("MemberId")
-            if(postId!=null) findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToPostFragment(postId = postId))
-            else if(memberId!=null) findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToProfileFragment(memberId = memberId))
+            if (postId != null) findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToPostFragment(
+                    postId = postId
+                )
+            )
+            else if (memberId != null) findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToProfileFragment(
+                    memberId = memberId
+                )
+            )
             else findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToNotificationFragment())
+        }
+    }
+
+    private val permissionLauncherNotification =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val deniedList: List<String> = permissions.filter {
+                !it.value
+            }.map {
+                it.key
+            }
+
+            when {
+                deniedList.isNotEmpty() -> {
+                    DayoApplication.preferences.notiDevicePermit = false
+                    DayoApplication.preferences.notiNoticePermit = false
+                    val map = deniedList.groupBy { permission ->
+                        if (shouldShowRequestPermissionRationale(permission)) getString(R.string.permission_fail_second) else getString(
+                            R.string.permission_fail_final
+                        )
+                    }
+                    map[getString(R.string.permission_fail_second)]?.let {
+                        // request denied , request again
+                        Toast.makeText(
+                            this,
+                            getString(R.string.permission_fail_message_notification),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        ActivityCompat.requestPermissions(
+                            this,
+                            notificationPermission,
+                            1000
+                        )
+                    }
+                    map[getString(R.string.permission_fail_final)]?.let {
+                        Intent().apply {
+                            action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                        }
+                        Toast.makeText(
+                            this,
+                            getString(R.string.permission_fail_final_message_notification),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                else -> {
+                    //All request are permitted
+                    // 알림 최초 허용시에 모든 알림 허용처리
+                    DayoApplication.preferences.notiDevicePermit = true
+                    DayoApplication.preferences.notiNoticePermit = true
+                    settingNotificationViewModel.registerDeviceToken()
+                    settingNotificationViewModel.requestReceiveAlarm()
+                }
+            }
+        }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                // by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                // "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                // If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                permissionLauncherNotification.launch(notificationPermission)
+            }
         }
     }
 
@@ -44,7 +152,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun findNavController(): NavController {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         return navHostFragment.navController
     }
 
@@ -60,7 +169,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         // WriteFragment
-        binding.bottomNavigationMainBar.setItemOnTouchListener( R.id.WriteFragment,
+        binding.bottomNavigationMainBar.setItemOnTouchListener(R.id.WriteFragment,
             object : View.OnTouchListener {
                 var rect = Rect()
                 var isInside = true
@@ -74,7 +183,8 @@ class MainActivity : AppCompatActivity() {
                             return true
                         }
                         MotionEvent.ACTION_MOVE -> {
-                            isInside = rect.contains(v!!.left + event.x.toInt(), v.top + event.y.toInt())
+                            isInside =
+                                rect.contains(v!!.left + event.x.toInt(), v.top + event.y.toInt())
                             binding.bottomNavigationMainBar.clearFocus()
                             return false
                         }
@@ -104,5 +214,9 @@ class MainActivity : AppCompatActivity() {
                 true
             }
         }
+    }
+
+    companion object {
+        val notificationPermission = arrayOf(Manifest.permission.POST_NOTIFICATIONS)
     }
 }
