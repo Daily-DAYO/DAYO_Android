14b407b3d693e0c8024e3fea6010a99b2fb7b277
DongJun-H <dongjun826@seoultech.ac.kr>
2023-08-10 11:42:39 +0900

    [feature] Add Compose DP unit conversion

--- app/src/main/java/com/daily/dayo/common/ConvertDP.kt
+++ app/src/main/java/com/daily/dayo/common/ConvertDP.kt
@@ -2,6 +2,9 @@ package com.daily.dayo.common
 
 import android.content.res.Resources
 import android.util.TypedValue
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 
 val Int.dp: Int
     get() {
@@ -12,3 +15,6 @@ val Int.dp: Int
 
 val Float.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+@Composable
+fun Dp.toSp() = with(LocalDensity.current) { this@toSp.toSp() }
