c15f66f1b1219911f4832d18b7df844f6a56bb1e
Ju YunGyeom <joo_930@naver.com>
2024-02-21 18:46:10 +0900

    #546 [layout] Set bottom sheet dialog to appear above bottom navigation bar

--- presentation/src/main/java/daily/dayo/presentation/screen/home/HomeScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package daily.dayo.presentation.screen.home
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -30,19 +32,28 @@ import daily.dayo.presentation.fragment.home.HOME_DAYOPICK_PAGE_TAB_ID
 import daily.dayo.presentation.fragment.home.HOME_NEW_PAGE_TAB_ID
 import daily.dayo.presentation.theme.Gray1_313131
 import daily.dayo.presentation.theme.Gray5_E8EAEE
+import daily.dayo.presentation.view.BottomSheetDialog
 import daily.dayo.presentation.view.TextButton
 import daily.dayo.presentation.view.TopNavigation
 import daily.dayo.presentation.view.getBottomSheetDialogState
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     navController: NavController,
     coroutineScope: CoroutineScope,
-    bottomSheetState: ModalBottomSheetState
+    bottomSheetState: ModalBottomSheetState,
+    bottomSheetContent: (@Composable () -> Unit) -> Unit
 ) {
     var homeTabState by rememberSaveable { mutableIntStateOf(HOME_DAYOPICK_PAGE_TAB_ID) }
+    var selectedCategory by rememberSaveable { mutableStateOf(Pair(CategoryMenu.All.name, 0)) } // name, index
+    val onCategorySelected: (String, Int) -> Unit = { category, index ->
+        selectedCategory = Pair(category, index)
+        coroutineScope.launch { bottomSheetState.hide() }
+    }
+
     Scaffold(
         topBar = {
             TopNavigation(
@@ -90,9 +101,47 @@ fun HomeScreen(
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
-
+            HomeDayoPickScreen(selectedCategory.first, coroutineScope, bottomSheetState)
         }
     }
+
+    bottomSheetContent {
+        CategoryBottomSheetDialog(onCategorySelected, selectedCategory, coroutineScope, bottomSheetState)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun CategoryBottomSheetDialog(
+    onCategorySelected: (String, Int) -> Unit,
+    selectedCategory: Pair<String, Int>,
+    coroutineScope: CoroutineScope,
+    bottomSheetState: ModalBottomSheetState
+) {
+    val categoryMenus = listOf(
+        CategoryMenu.All,
+        CategoryMenu.Scheduler,
+        CategoryMenu.StudyPlanner,
+        CategoryMenu.PocketBook,
+        CategoryMenu.SixHoleDiary,
+        CategoryMenu.Digital,
+        CategoryMenu.ETC
+    )
+
+    BottomSheetDialog(
+        sheetState = bottomSheetState,
+        buttons = categoryMenus.mapIndexed { index, category ->
+            Pair(category.name) {
+                onCategorySelected(category.name, index)
+            }
+        },
+        title = stringResource(id = R.string.filter),
+        leftIconButtons = categoryMenus.map {
+            ImageVector.vectorResource(it.defaultIcon)
+        },
+        checkedButtonIndex = selectedCategory.second,
+        closeButtonAction = { coroutineScope.launch { bottomSheetState.hide() } }
+    )
 }
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -100,6 +149,17 @@ fun HomeScreen(
 @Preview(showBackground = true)
 private fun PreviewHomeScreen() {
     MaterialTheme {
-        HomeScreen(rememberNavController(), rememberCoroutineScope(), getBottomSheetDialogState())
+        HomeScreen(rememberNavController(), rememberCoroutineScope(), getBottomSheetDialogState(), {})
     }
+}
+
+
+sealed class CategoryMenu(val name: String, @DrawableRes val defaultIcon: Int) {
+    object All : CategoryMenu("전체", R.drawable.ic_category_all)
+    object Scheduler : CategoryMenu("스케줄러", R.drawable.ic_category_scheduler)
+    object StudyPlanner : CategoryMenu("스터디 플래너", R.drawable.ic_category_studyplanner)
+    object PocketBook : CategoryMenu("포켓북", R.drawable.ic_category_pocketbook)
+    object SixHoleDiary : CategoryMenu("6공 다이어리", R.drawable.ic_category_sixholediary)
+    object Digital : CategoryMenu("모바일 다이어리", R.drawable.ic_category_digital)
+    object ETC : CategoryMenu("기타", R.drawable.ic_category_etc)
 }
