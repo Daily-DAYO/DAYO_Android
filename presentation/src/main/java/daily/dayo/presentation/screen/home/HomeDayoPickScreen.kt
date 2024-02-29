51c04bde57a6fe68d53ec8cfecb54a31ddc435ed
Ju YunGyeom <joo_930@naver.com>
2024-03-01 01:51:51 +0900

    #546 [layout] Home Empty View

--- presentation/src/main/java/daily/dayo/presentation/screen/home/HomeDayoPickScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/home/HomeDayoPickScreen.kt
@@ -1,13 +1,16 @@
 package daily.dayo.presentation.screen.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -29,20 +32,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import daily.dayo.presentation.R
 import daily.dayo.presentation.common.Status
 import daily.dayo.presentation.theme.Gray2_767B83
+import daily.dayo.presentation.theme.Gray3_9FA5AE
+import daily.dayo.presentation.theme.Gray4_C5CAD2
 import daily.dayo.presentation.theme.Gray6_F0F1F3
 import daily.dayo.presentation.theme.White_FFFFFF
+import daily.dayo.presentation.theme.b3
+import daily.dayo.presentation.theme.caption1
 import daily.dayo.presentation.theme.caption3
 import daily.dayo.presentation.view.EmojiView
+import daily.dayo.presentation.view.FilledButton
 import daily.dayo.presentation.view.HomePostView
 import daily.dayo.presentation.viewmodel.HomeViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -59,83 +72,114 @@ fun HomeDayoPickScreen(
     val dayoPickPostList = homeViewModel.dayoPickPostList.observeAsState()
     val refreshing by homeViewModel.isRefreshing.collectAsStateWithLifecycle()
     val pullRefreshState = rememberPullRefreshState(refreshing, { homeViewModel.loadDayoPickPosts() })
-    Box(Modifier.pullRefresh(pullRefreshState)) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-            contentPadding = PaddingValues(horizontal = 18.dp),
-            modifier = Modifier
-                .fillMaxSize()
-                .pullRefresh(pullRefreshState)
-        ) {
-            // description
-            item(span = { GridItemSpan(2) }) {
-                Row(
-                    modifier = Modifier
-                        .wrapContentHeight()
-                        .fillMaxWidth()
-                        .padding(top = 8.dp, bottom = 12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Row {
-                        EmojiView(
-                            emoji = "\uD83D\uDCA1",
-                            emojiSize = MaterialTheme.typography.bodyMedium.fontSize,
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
+    var isEmpty by rememberSaveable { mutableStateOf(false) }
 
-                        Text(
-                            text = stringResource(id = R.string.home_dayopick_description),
-                            style = MaterialTheme.typography.bodyMedium.copy(Color(0xFF73777C)),
-                            modifier = Modifier
-                                .padding(horizontal = 4.dp)
-                                .align(Alignment.CenterVertically)
-                        )
-                    }
-                    CategoryButton(selectedCategoryName, coroutineScope, bottomSheetState)
-                }
-            }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState)
+    ) {
+        Column {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                contentPadding = PaddingValues(horizontal = 18.dp),
+                modifier = Modifier.wrapContentHeight()
+            ) {
+                // description
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    Row(
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth()
+                            .padding(top = 8.dp, bottom = 12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row {
+                            EmojiView(
+                                emoji = "\uD83D\uDCA1",
+                                emojiSize = MaterialTheme.typography.bodyMedium.fontSize,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
 
-            // dayo pick list
-            when (dayoPickPostList.value?.status) {
-                Status.SUCCESS -> {
-                    dayoPickPostList.value?.data?.mapIndexed { index, post ->
-                        item {
-                            HomePostView(
-                                post = post,
-                                isDayoPick = index in 0..4,
-                                modifier = Modifier.padding(bottom = 20.dp),
-                                onClickPost = {
-                                    // todo move to post
-                                },
-                                onClickLikePost = {
-                                    if (!post.heart) {
-                                        homeViewModel.requestLikePost(post.postId!!, isDayoPickLike = true)
-                                    } else {
-                                        homeViewModel.requestUnlikePost(post.postId!!, isDayoPickLike = true)
-                                    }
-                                },
-                                onClickNickname = {
-                                    // todo move to profile
-                                }
+                            Text(
+                                text = stringResource(id = R.string.home_dayopick_description),
+                                style = MaterialTheme.typography.bodyMedium.copy(Color(0xFF73777C)),
+                                modifier = Modifier
+                                    .padding(horizontal = 4.dp)
+                                    .align(Alignment.CenterVertically)
                             )
                         }
+                        CategoryButton(selectedCategoryName, coroutineScope, bottomSheetState)
                     }
                 }
 
-                Status.LOADING -> {
+                // dayo pick list
+                when (dayoPickPostList.value?.status) {
+                    Status.SUCCESS -> {
+                        isEmpty = dayoPickPostList.value?.data?.isEmpty() == true
+                        dayoPickPostList.value?.data?.mapIndexed { index, post ->
+                            item {
+                                HomePostView(
+                                    post = post,
+                                    isDayoPick = index in 0..4,
+                                    modifier = Modifier.padding(bottom = 20.dp),
+                                    onClickPost = {
+                                        // todo move to post
+                                    },
+                                    onClickLikePost = {
+                                        if (!post.heart) {
+                                            homeViewModel.requestLikePost(post.postId!!, isDayoPickLike = true)
+                                        } else {
+                                            homeViewModel.requestUnlikePost(post.postId!!, isDayoPickLike = true)
+                                        }
+                                    },
+                                    onClickNickname = {
+                                        // todo move to profile
+                                    }
+                                )
+                            }
+                        }
+                    }
+
+                    Status.LOADING -> {
+                    }
+
+                    Status.ERROR -> {
+
+                    }
+
+                    else -> {}
                 }
-
-                Status.ERROR -> {
-
-                }
-
-                else -> {}
             }
+
+            // empty view
+            if (isEmpty) HomeDayoPickEmptyView()
         }
 
+        // refresh indicator
         PullRefreshIndicator(refreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
+    }
+}
+
+@Composable
+private fun HomeDayoPickEmptyView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_home_empty), contentDescription = null)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = stringResource(id = R.string.home_dayopick_empty_title), style = MaterialTheme.typography.b3.copy(Gray3_9FA5AE))
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = stringResource(id = R.string.home_dayopick_empty_detail), style = MaterialTheme.typography.caption1.copy(Gray4_C5CAD2))
+
+        Spacer(modifier = Modifier.height(28.dp))
+        FilledButton(onClick = { /*TODO*/ }, label = stringResource(id = R.string.home_dayopick_empty_action))
     }
 }
 
