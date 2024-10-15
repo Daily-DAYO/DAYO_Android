7ff63efdb8da88c4159ebcfa5237176086e864f6
Ju Yungyeom <joo_930@naver.com>
2024-10-15 21:42:14 +0900

    [layout] Bookmark Header

--- presentation/src/main/java/daily/dayo/presentation/screen/bookmark/BookmarkScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/bookmark/BookmarkScreen.kt
@@ -4,28 +4,43 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Text
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import daily.dayo.presentation.R
 import daily.dayo.presentation.theme.Gray1_50545B
+import daily.dayo.presentation.theme.Gray2_767B83
+import daily.dayo.presentation.theme.Primary_23C882
 import daily.dayo.presentation.theme.White_FFFFFF
+import daily.dayo.presentation.theme.caption2
+import daily.dayo.presentation.theme.caption4
 import daily.dayo.presentation.view.TopNavigation
 import daily.dayo.presentation.view.TopNavigationAlign
+import daily.dayo.presentation.viewmodel.ProfileViewModel
+import java.text.DecimalFormat
 
 @Composable
 fun BookmarkScreen(
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    profileViewModel: ProfileViewModel = hiltViewModel()
 ) {
     Scaffold(
         topBar = { BookmarkTopNavigation(onBackClick) },
@@ -37,9 +52,10 @@ fun BookmarkScreen(
                     .background(color = White_FFFFFF)
                     .fillMaxSize()
                     .padding(contentPadding)
-                    .padding(horizontal = 20.dp),
             ) {
-
+                item(span = { GridItemSpan(2) }) {
+                    BookmarkHeader()
+                }
             }
         }
     )
@@ -68,4 +84,48 @@ private fun BookmarkTopNavigation(
         title = stringResource(id = R.string.bookmark),
         titleAlignment = TopNavigationAlign.CENTER
     )
+}
+
+@Composable
+private fun BookmarkHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .padding(start = 18.dp, end = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            val dec = DecimalFormat("#,###")
+            Text(
+                text = " ${dec.format(0)} ",
+                style = MaterialTheme.typography.caption2.copy(Primary_23C882),
+            )
+            Text(
+                text = stringResource(id = R.string.bookmark_count),
+                style = MaterialTheme.typography.caption2.copy(Gray2_767B83)
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp),
+                text = stringResource(id = R.string.bookmark_edit),
+                style = MaterialTheme.typography.caption4.copy(color = Gray1_50545B),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewBookmarkHeader() {
+    BookmarkHeader()
 }
