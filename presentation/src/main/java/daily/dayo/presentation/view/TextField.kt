7691f5f7bee3a9367854c83be63fb5812dd4f6bb
Ju Yungyeom <joo_930@naver.com>
2024-11-09 17:33:44 +0900

    [layout] Renewal DayoTextField

--- presentation/src/main/java/daily/dayo/presentation/view/TextField.kt
+++ presentation/src/main/java/daily/dayo/presentation/view/TextField.kt
@@ -1,12 +1,23 @@
 package daily.dayo.presentation.view
 
 import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -24,17 +35,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import daily.dayo.presentation.R
 import daily.dayo.presentation.common.TextLimitUtil
 import daily.dayo.presentation.theme.Dark
 import daily.dayo.presentation.theme.Gray2_767B83
@@ -44,8 +60,123 @@ import daily.dayo.presentation.theme.Gray6_F0F1F3
 import daily.dayo.presentation.theme.Gray7_F6F6F7
 import daily.dayo.presentation.theme.Primary_23C882
 import daily.dayo.presentation.theme.Red_FF4545
+import daily.dayo.presentation.theme.b4
 import daily.dayo.presentation.theme.b6
+import daily.dayo.presentation.theme.caption3
+import daily.dayo.presentation.theme.caption4
 import kotlinx.coroutines.delay
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DayoTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(top = 0.dp, bottom = 8.dp),
+    label: String = "",
+    placeholder: String = "",
+    @DrawableRes trailingIconId: Int? = null,
+    isError: Boolean? = null,
+    errorMessage: String = "",
+    textAlign: TextAlign = TextAlign.Left,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    onTrailingIconClick: (() -> Unit) = { }
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        if (label.isNotEmpty()) {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.caption3.copy(
+                    color = Gray4_C5CAD2,
+                    fontWeight = FontWeight.SemiBold
+                )
+            )
+        }
+
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier.align(alignment = Alignment.CenterEnd)
+            ) {
+                Column {
+                    if (isError != null && isError == true) {
+                        Icon(
+                            imageVector = Icons.Filled.Cancel,
+                            contentDescription = "error",
+                            modifier = Modifier
+                                .size(20.dp)
+                                .clickable(onClick = onTrailingIconClick),
+                            tint = Red_FF4545
+                        )
+                    } else if (trailingIconId != null) {
+                        Icon(
+                            painter = painterResource(id = trailingIconId),
+                            contentDescription = "error",
+                            modifier = Modifier
+                                .size(20.dp)
+                                .clickable(onClick = onTrailingIconClick),
+                            tint = Color.Unspecified
+                        )
+                    }
+                }
+            }
+
+            BasicTextField(
+                value = value,
+                onValueChange = onValueChange,
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                textStyle = TextStyle(textAlign = textAlign, color = Dark),
+                interactionSource = interactionSource,
+                decorationBox = @Composable { innerTextField ->
+                    TextFieldDefaults.DecorationBox(
+                        value = value,
+                        visualTransformation = VisualTransformation.None,
+                        innerTextField = innerTextField,
+                        placeholder = {
+                            Text(
+                                text = placeholder,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                style = MaterialTheme.typography.b4.copy(Gray4_C5CAD2)
+                            )
+                        },
+                        singleLine = true,
+                        isError = isError ?: false,
+                        enabled = true,
+                        interactionSource = interactionSource,
+                        contentPadding = contentPadding,
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Primary_23C882, // 밑줄
+                            unfocusedIndicatorColor = Gray6_F0F1F3,
+                            errorIndicatorColor = Red_FF4545,
+                            unfocusedContainerColor = Color.Transparent, // 배경
+                            disabledContainerColor = Color.Transparent,
+                            errorContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedLabelColor = Color.Transparent, // 라벨
+                            focusedLabelColor = Gray4_C5CAD2,
+                            errorLabelColor = Red_FF4545,
+                            focusedPlaceholderColor = Gray5_E8EAEE, // 힌트
+                            unfocusedPlaceholderColor = Gray5_E8EAEE,
+                            disabledPlaceholderColor = Gray5_E8EAEE
+                        )
+                    )
+                }
+            )
+        }
+
+        if (isError != null) {
+            Text(
+                text = if (isError) errorMessage else "",
+                modifier = Modifier.padding(top = 4.dp),
+                style = MaterialTheme.typography.caption4.copy(Red_FF4545)
+            )
+        }
+    }
+}
 
 @Composable
 fun FilledTextField(
@@ -239,14 +370,17 @@ fun CharacterLimitOutlinedTextField(
 @Preview(showBackground = true)
 @Composable
 private fun PreviewTextField() {
-    Column {
+    Column(Modifier.fillMaxWidth()) {
         // Default 사용 예시
         val text = rememberSaveable { mutableStateOf("") }
         val isError = text.value == "error"
-        FilledTextField(
+
+        DayoTextField(
             value = text.value,
             onValueChange = { textValue -> text.value = textValue },
-            label = "Text",
+            modifier = Modifier.padding(horizontal = 16.dp),
+            label = "label",
+            trailingIconId = R.drawable.ic_img_delete_gray_5,
             isError = isError,
             errorMessage = "error",
             placeholder = "text를 입력하세요"
