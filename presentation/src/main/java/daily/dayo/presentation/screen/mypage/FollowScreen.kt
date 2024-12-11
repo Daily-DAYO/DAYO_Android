5c92368bfdeb1befcccd270b0a96c58d39dd247e
Ju Yungyeom <joo_930@naver.com>
2024-12-11 23:27:02 +0900

    #592 [feature] follower, following empty view

--- presentation/src/main/java/daily/dayo/presentation/screen/mypage/FollowScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/mypage/FollowScreen.kt
@@ -1,11 +1,16 @@
 package daily.dayo.presentation.screen.mypage
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
@@ -29,11 +34,14 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -41,9 +49,13 @@ import daily.dayo.domain.model.Profile
 import daily.dayo.presentation.R
 import daily.dayo.presentation.theme.Gray1_50545B
 import daily.dayo.presentation.theme.Gray2_767B83
+import daily.dayo.presentation.theme.Gray3_9FA5AE
+import daily.dayo.presentation.theme.Gray4_C5CAD2
 import daily.dayo.presentation.theme.Primary_23C882
 import daily.dayo.presentation.theme.White_FFFFFF
+import daily.dayo.presentation.theme.b3
 import daily.dayo.presentation.theme.b5
+import daily.dayo.presentation.theme.caption2
 import daily.dayo.presentation.view.TopNavigation
 import daily.dayo.presentation.view.TopNavigationAlign
 import daily.dayo.presentation.viewmodel.FollowUiState
@@ -66,6 +78,8 @@ internal fun FollowScreen(
 
     LaunchedEffect(memberId) {
         profileViewModel.requestOtherProfile(memberId)
+        followViewModel.requestFollowerList(memberId)
+        followViewModel.requestFollowingList(memberId)
     }
 
     LaunchedEffect(pagerState) {
@@ -107,7 +121,7 @@ private fun FollowScreen(
                     ) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_back_sign),
-                            contentDescription = "back sign",
+                            contentDescription = stringResource(id = R.string.back_sign),
                             tint = Gray1_50545B
                         )
                     }
@@ -121,15 +135,27 @@ private fun FollowScreen(
         Column(
             modifier = Modifier.padding(innerPadding),
         ) {
-            FollowTab(pagerState = pagerState)
+            val followerCount = when (followerUiState) {
+                is FollowUiState.Success -> followerUiState.count
+                is FollowUiState.Loading,
+                is FollowUiState.Error -> 0
+            }
+
+            val followingCount = when (followingUiState) {
+                is FollowUiState.Success -> followingUiState.count
+                is FollowUiState.Loading,
+                is FollowUiState.Error -> 0
+            }
+
+            FollowTab(pagerState, followerCount, followingCount)
 
             HorizontalPager(
                 state = pagerState,
                 flingBehavior = PagerDefaults.flingBehavior(state = pagerState),
                 pageContent = { page ->
                     when (page) {
-                        FOLLOWER_TAB_ID -> FollowerContent(followerUiState)
-                        FOLLOWING_TAB_ID -> FollowingContent(followingUiState)
+                        FOLLOWER_TAB_ID -> FollowContent(page, followerUiState)
+                        FOLLOWING_TAB_ID -> FollowContent(page, followingUiState)
                     }
                 }
             )
@@ -138,7 +164,7 @@ private fun FollowScreen(
 }
 
 @Composable
-private fun FollowerContent(followUiState: FollowUiState) {
+private fun FollowContent(tabNum: Int, followUiState: FollowUiState) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -150,27 +176,86 @@ private fun FollowerContent(followUiState: FollowUiState) {
             }
 
             is FollowUiState.Success -> {
-                items(followUiState.data.size) { index ->
-                    val item = followUiState.data[index]
-                    Text(item.nickname)
+                if (followUiState.data.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier.fillParentMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            FollowerEmpty(tabNum)
+                        }
+                    }
+                } else {
+                    items(followUiState.data.size) { index ->
+                        val item = followUiState.data[index]
+                        Text(item.nickname)
+                    }
                 }
             }
 
             is FollowUiState.Error -> {
-
+                item {
+                    Box(
+                        modifier = Modifier.fillParentMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        FollowerEmpty(tabNum)
+                    }
+                }
             }
         }
-
     }
 }
 
 @Composable
-private fun FollowingContent(followUiState: FollowUiState) {
+private fun FollowerEmpty(tabNum: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_follow_empty),
+                contentDescription = stringResource(id = R.string.follower_empty_description)
+            )
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = stringResource(
+                    if (tabNum == FOLLOWER_TAB_ID) R.string.follower_empty_description
+                    else R.string.following_empty_description
+                ),
+                modifier = Modifier.padding(bottom = 2.dp),
+                color = Gray3_9FA5AE,
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Visible,
+                style = MaterialTheme.typography.b3,
+            )
+
+            Text(
+                text = stringResource(
+                    if (tabNum == FOLLOWER_TAB_ID) R.string.follower_empty_sub_description
+                    else R.string.following_empty_sub_description
+                ),
+                color = Gray4_C5CAD2,
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Visible,
+                style = MaterialTheme.typography.caption2,
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+    }
 }
 
 @Composable
-private fun FollowTab(pagerState: PagerState) {
+private fun FollowTab(pagerState: PagerState, followerCount: Int, followingCount: Int) {
     val coroutineScope = rememberCoroutineScope()
     val pages = listOf(
         stringResource(id = R.string.follower),
@@ -190,13 +275,13 @@ private fun FollowTab(pagerState: PagerState) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 18.dp)
-            .padding(top = 4.dp),
+            .padding(top = 4.dp)
     ) {
         pages.forEachIndexed { index, title ->
             Tab(
                 text = {
                     Text(
-                        text = title,
+                        text = "$title " + if (index == FOLLOWER_TAB_ID) "$followerCount" else "$followingCount",
                         style = MaterialTheme.typography.b5
                     )
                 },
@@ -207,7 +292,7 @@ private fun FollowTab(pagerState: PagerState) {
                     coroutineScope.launch {
                         pagerState.animateScrollToPage(index)
                     }
-                },
+                }
             )
         }
     }
