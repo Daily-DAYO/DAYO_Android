18198d00a15ff201c7f861e5afcbdd6a9357eb51
DongJun Huh <dongjun826@seoultech.ac.kr>
2024-12-15 23:52:27 +0900

    [refactoring] Migrate Write-related logics XML to Compose

--- presentation/src/main/java/daily/dayo/presentation/screen/write/WriteNavigation.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/write/WriteNavigation.kt
@@ -1,18 +1,100 @@
 package daily.dayo.presentation.screen.write
 
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 
+fun NavController.navigateWrite() {
+    navigate(WriteRoute.route)
+}
+
+fun NavController.navigateWriteTag() {
+    navigate(WriteRoute.tag)
+}
+
+fun NavController.navigateWriteFolder() {
+    navigate(WriteRoute.folder)
+}
+fun NavController.navigateWriteFolderNew() {
+    navigate(WriteRoute.folderNew)
+}
+
+@OptIn(ExperimentalMaterialApi::class)
 fun NavGraphBuilder.writeNavGraph(
-    onBackClick: () -> Unit
+    snackBarHostState: SnackbarHostState,
+    navController: NavController,
+    onBackClick: () -> Unit,
+    onTagClick: () -> Unit,
+    onWriteFolderClick: () -> Unit,
+    onWriteFolderNewClick: () -> Unit,
+    bottomSheetState: ModalBottomSheetState,
+    bottomSheetContent: (@Composable () -> Unit) -> Unit,
 ) {
-    composable(route = WriteRoute.route) {
-        WriteRoute(
-            onBackClick = onBackClick,
-        )
+    navigation(
+        startDestination = WriteRoute.route,
+        route = "writeNavigation"
+    ) {
+        composable(route = WriteRoute.route) {
+            val parentStackEntry = remember(it) {
+                navController.getBackStackEntry(WriteRoute.route)
+            }
+            WriteRoute(
+                snackBarHostState = snackBarHostState,
+                onBackClick = onBackClick,
+                onTagClick = onTagClick,
+                onWriteFolderClick = onWriteFolderClick,
+                writeViewModel = hiltViewModel(parentStackEntry),
+                bottomSheetState = bottomSheetState,
+                bottomSheetContent = bottomSheetContent
+            )
+        }
+
+        composable(route = WriteRoute.tag) {
+            val parentStackEntry = remember(it) {
+                navController.getBackStackEntry(WriteRoute.route)
+            }
+            WriteTagRoute(
+                onBackClick = onBackClick,
+                writeViewModel = hiltViewModel(parentStackEntry),
+            )
+        }
+
+        composable(route = WriteRoute.folder) {
+            val parentStackEntry = remember(it) {
+                navController.getBackStackEntry(WriteRoute.route)
+            }
+            WriteFolderRoute(
+                onBackClick = onBackClick,
+                onWriteFolderNewClick = onWriteFolderNewClick,
+                writeViewModel = hiltViewModel(parentStackEntry),
+                profileViewModel = hiltViewModel(parentStackEntry),
+                accountViewModel = hiltViewModel(parentStackEntry),
+            )
+        }
+
+        composable(route = WriteRoute.folderNew) {
+            val parentStackEntry = remember(it) {
+                navController.getBackStackEntry(WriteRoute.route)
+            }
+            WriteFolderNewRoute(
+                onBackClick = onBackClick,
+                writeViewModel = hiltViewModel(parentStackEntry),
+            )
+        }
     }
 }
 
 object WriteRoute {
     const val route = "write"
+
+    const val tag = "${route}/tag"
+    const val folder = "${route}/folder"
+    const val folderNew = "${route}/folder/new"
 }
