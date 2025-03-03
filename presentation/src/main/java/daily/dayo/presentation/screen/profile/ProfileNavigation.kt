4193f233db54dbde352ffdb0559e055033b9551a
Ju Yungyeom <joo_930@naver.com>
2025-03-03 16:49:28 +0900

    #624 [feature] Add Profile Navigation

---
+++ presentation/src/main/java/daily/dayo/presentation/screen/profile/ProfileNavigation.kt
@@ -0,0 +1,41 @@
+package daily.dayo.presentation.screen.profile
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+fun NavController.navigateProfile(memberId: String) {
+    navigate(ProfileRoute.profile(memberId))
+}
+
+fun NavGraphBuilder.profileNavGraph(
+    onFollowMenuClick: (String, Int) -> Unit,
+    onFolderClick: (String) -> Unit,
+    onPostClick: (String) -> Unit,
+    onBackClick: () -> Unit
+) {
+    composable(
+        route = ProfileRoute.profile("{memberId}"),
+        arguments = listOf(
+            navArgument("memberId") {
+                type = NavType.StringType
+            }
+        )
+    ) { navBackStackEntry ->
+        val memberId = navBackStackEntry.arguments?.getString("memberId") ?: ""
+        ProfileScreen(
+            memberId = memberId,
+            onFollowMenuClick = onFollowMenuClick,
+            onFolderClick = onFolderClick,
+            onPostClick = onPostClick,
+            onBackClick = onBackClick
+        )
+    }
+}
+
+object ProfileRoute {
+    const val route = "profile"
+    fun profile(memberId: String) = "$route/$memberId"
+}
---
+++ presentation/src/main/java/daily/dayo/presentation/screen/profile/ProfileScreen.kt
@@ -0,0 +1,14 @@
+package daily.dayo.presentation.screen.profile
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen(
+    memberId: String,
+    onFollowMenuClick: (String, Int) -> Unit,
+    onFolderClick: (String) -> Unit,
+    onPostClick: (String) -> Unit,
+    onBackClick: () -> Unit
+) {
+
+}
