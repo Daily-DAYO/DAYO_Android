65ca6190e40b48344fcd9fdb1ef1daa44fd7678d
Ju Yungyeom <joo_930@naver.com>
2025-10-26 20:58:11 +0900

    #695 [feature] Double back press to exit app on MainScreen

--- presentation/src/main/java/daily/dayo/presentation/screen/main/MainScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/main/MainScreen.kt
@@ -1,5 +1,7 @@
 package daily.dayo.presentation.screen.main
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.ExperimentalSharedTransitionApi
@@ -30,6 +32,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -37,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,6 +82,7 @@ import kotlinx.coroutines.launch
 @Composable
 internal fun MainScreen(
     onAdRequest: (onRewardSuccess: () -> Unit) -> Unit,
+    onExit: () -> Unit,
     navigator: MainNavigator = rememberMainNavigator(),
     profileViewModel: ProfileViewModel = hiltViewModel()
 ) {
@@ -92,6 +97,22 @@ internal fun MainScreen(
         derivedStateOf { if (bottomSheetState.bottomSheetState.currentValue == SheetValue.Expanded) 0.6f else 0f }
     }
     val animatedDimAlpha by animateFloatAsState(targetValue = bottomSheetDimAlpha)
+    val context = LocalContext.current
+    var lastBackPressedTime by remember { mutableLongStateOf(0L) }
+    val toast = remember { Toast.makeText(context, context.getString(R.string.main_finish_toast), Toast.LENGTH_SHORT) }
+
+    BackHandler {
+        if (!navigator.navigateUp()) { // 뒤로갈 화면 없는 경우
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastBackPressedTime < 2000) {
+                toast?.cancel()
+                onExit() // 앱 종료
+            } else {
+                lastBackPressedTime = currentTime
+                toast?.show()
+            }
+        }
+    }
 
     SharedTransitionLayout {
         BottomSheetScaffold(
