103188a4e5f2a143c2ba2bb548bf75b396d1556c
Ju Yungyeom <joo_930@naver.com>
2025-11-19 20:19:08 +0900

    #705 [layout] Remove email display in profile

--- presentation/src/main/java/daily/dayo/presentation/screen/mypage/MyPageScreen.kt
+++ presentation/src/main/java/daily/dayo/presentation/screen/mypage/MyPageScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -171,27 +172,19 @@ private fun MyPageProfile(
                 )
         )
 
-        // nickname, email
-        Column(
+        // nickname
+        Text(
+            text = profile?.nickname ?: "",
             modifier = Modifier
                 .weight(1f)
-                .padding(start = 8.dp)
-        ) {
-            Text(
-                text = profile?.nickname ?: "",
-                style = DayoTheme.typography.h1.copy(
-                    color = Dark,
-                    fontWeight = FontWeight.SemiBold
-                )
+                .padding(horizontal = 8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = DayoTheme.typography.h1.copy(
+                color = Dark,
+                fontWeight = FontWeight.SemiBold
             )
-
-            Text(
-                text = profile?.email ?: "",
-                style = DayoTheme.typography.caption5.copy(
-                    color = Gray4_C5CAD2
-                )
-            )
-        }
+        )
 
         // follower
         Column(
